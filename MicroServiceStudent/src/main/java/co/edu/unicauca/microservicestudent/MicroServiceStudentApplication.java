8cdde248ffa6eb96bec72fb6b728d2c3a48a30ab
David <eversonrestrepo@unicauca.edu.co>
2025-04-06 16:50:05 -0500

    carpetas de los microservicios

---
+++ MicroServiceStudent/src/main/java/co/edu/unicauca/microservicestudent/MicroServiceStudentApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.microservicestudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceStudentApplication.class, args);
+    }
+
+}
